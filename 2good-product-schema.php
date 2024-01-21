38fc678f5cd7cd004a8910371e4fd20074f74eaa
2GOOD <2GOOD@users.noreply.github.com>
2024-01-21 15:04:38 +0200

    Update plugin info, remove default woo shcema
    Remove the default schema

--- 2good-product-schema.php
+++ 2good-product-schema.php
@@ -1,14 +1,16 @@
 <?php
-/*
-
-    Plugin Name: 2GOOD Product Schema
-    Description: Adds 2GOOD schema markup to WooCommerce product pages.
-    Version: 1.0.0
-    Author: 2GOOD Tech LTD
-    License: GPLv2 or later
-    License URI: http://www.gnu.org/licenses/gpl-2.0.html
-    
-*/
+/**
+ * Plugin Name: 2GOOD Product Schema
+ * Plugin URI: 
+ * Description: Adds Product structured data schema markup to WooCommerce.
+ * Author: 2GOOD Technologies Ltd.
+ * Author URI: https://2good.tech
+ * Version: 1.0.1
+ *
+ * License: GNU General Public License v3.0
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.html
+ *
+ */
 
 if (!defined('ABSPATH')) {
     exit; // Exit if accessed directly
@@ -24,8 +26,9 @@ add_action('template_redirect', 'load_2good_product_schema');
 function load_2good_product_schema() {
 	
     if (is_product_page()) {
-        // Everything currently in v1.1 is handled in the /includes/main.php file
-
+        // First remove the Woo default schema
+        remove_action( 'wp_footer', array( WC()->structured_data, 'output_structured_data' ), 10 );
+        // Everything else is handled in the /includes/main.php file
         define('TWOGOOD_PRODUCT_SCHEMA_DIR', plugin_dir_path(__FILE__));
         include_once(TWOGOOD_PRODUCT_SCHEMA_DIR . 'includes/main.php');
     }
